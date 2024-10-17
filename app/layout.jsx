5aa47dc5dc077fb5466eeafd935cb01e116c10a8
Monkam jose <monkamjose41@gmail.com>
2024-10-17 11:50:50 +0200

    creation of the nav-bar for the mobile version of the website

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,8 +8,8 @@ import StairTransition from "./StairTransition";
 const Jetbrains_Mono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: '--font-jetbrainsMono'
-})
+  variable: "--font-jetbrainsMono",
+});
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
 //   variable: "--font-geist-sans",
@@ -30,10 +30,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={Jetbrains_Mono.variable}>
-        <Header/>
-        <StairTransition/>
+        <Header />
+
+        <StairTransition />
         <PageTransition>{children}</PageTransition>
-          
       </body>
     </html>
   );
